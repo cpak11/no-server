49656938eb85e75a07397f4a39b64105bf569990
cpak11 <c.pak81@gmail.com>
2017-09-14 17:51:13 -0400

    wow

--- js/cndserv.js
+++ js/cndserv.js
@@ -1,13 +1,38 @@
 angular.module('cndapp').service('cndserv', function($http){
-
+var self = this
   this.getworldnews = function() {
     return $http({
       method: "GET",
       url: 'https://newsapi.org/v1/sources?category=general&country=us',
     }).then(function(response) {
+      var promises = [];
+      var sources = response.data.sources.splice(0,6)
+      sources.forEach(function(source){
+        promises.push(self.getworldnewsarticles(source.id))
+      })
+      return Promise.all(promises).then(function(articleResponse){
+        var articles = [];
+        articleResponse.forEach(function(cur){
+            articles.push(cur.articles)
+        })
+        return sources.map(function(source, ind){
+          source.articles = articles[ind]
+          return source
+        })
+      });
+    })
+  };
+
+  this.getworldnewsarticles = function(id) {
+    return $http({
+      method: "GET",
+      url: 'https://newsapi.org/v1/articles?source=' + id + '&sortBy=top&apiKey=718f3f327c5348bb8c698f3e95eab7a6',
+    }).then(function(response) {
       return response.data;
     })
   };
+
+
 
 
   this.getbusiness = function() {
--- js/ctrl/ctrlworldnews.js
+++ js/ctrl/ctrlworldnews.js
@@ -1,8 +1,7 @@
 angular.module('cndapp').controller('ctrlworldnews', function($scope, cndserv){
 
   cndserv.getworldnews().then(function(response) {
-    console.log(response);
-    $scope.gwn = response.sources;
+    $scope.sources = response;
   })
 
 });
